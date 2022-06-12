2393cde04c800ede7625b27f6100f9f4291a6eb9
aithrajbouty <aithrabouty@gmail.com>
2022-06-12 21:48:38 +0700

    push for deploy

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "[server]"  > ~/.streamlit/config.toml 
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = true"  >> ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"aithrabouty@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
