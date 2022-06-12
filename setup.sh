9b4082a2098b5376ec4e5ac3fe40dfcaa5267569
aithrajbouty <aithrabouty@gmail.com>
2022-06-12 22:01:10 +0700

    update setup

--- setup.sh
+++ setup.sh
@@ -10,4 +10,12 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]\n\
+primaryColor=\"#529AEF\"\n\
+backgroundColor=\"#F1F5FB\"\n\
+secondaryBackgroundColor=\"#FFFFFF\"\n\
+textColor=\"#252733\"\n\
+font=\"sans serif\"\n\
+[global]\n\
+dataFrameSerialization = \"legacy\"\n\
 " > ~/.streamlit/config.toml
